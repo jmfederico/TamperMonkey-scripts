e2753c18ddb3f26414a67bd049d7e2a29fc30994
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2016-06-19 09:50:56 -0500

    Better number handling for BBVAnet

--- BBVAnet/twiks.user.js
+++ BBVAnet/twiks.user.js
@@ -52,10 +52,10 @@
     numeral.language('es');
     numeral.defaultFormat('0,0.00');
 
-    $('.pesetas:not(:has(*)), .pesetas-i:not(:has(*))').each(function () {
+    $('.pesetas:not(:has(*)), .pesetas-i:not(:has(*)), .pesetas1:not(:has(*)), .dato:not(:has(*))').each(function () {
         var $this = $(this);
 
-        if ($this.html().match(/[0-9]{1,3}\.[0-9]{2}$/) !== null) {
+        if ($this.html().match(/[0-9]{1,3},[0-9]{3}(\.[0-9]{2})?$/) !== null) {
             numeral.language('en');
             var valor = numeral($this.html());
             numeral.language('es');
