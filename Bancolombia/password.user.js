5cb56f8e9e9b28141ca7a53d6f46be5d5f446ab0
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2016-06-22 21:36:31 -0500

    Enable 2nd password autosubmit

--- Bancolombia/password.user.js
+++ Bancolombia/password.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Bancolombia password
 // @namespace    http://federicojm.com/
-// @version      0.1
+// @version      0.2
 // @description  Submit password filled by password manager.
 // @author       Federico JM
 // @match        https://sucursalpersonas.transaccionesbancolombia.com/mua/VALIDATEPASSWORD
+// @match        https://sucursalpersonas.transaccionesbancolombia.com/cb/pages/jsp/home/index.jsp
 // @grant        none
 // @run-at       document-end
 // ==/UserScript==
@@ -24,12 +25,13 @@
     // Password manager fills password field.
     // Bancolombia uses an onscreen keyboard that fails with password managers.
     // Simulate clicking each number and submit.
-    $('#password').change(function() {
+    $(document).on('change', "#password", function() {
         var password = $(this).val();
 
         // Bancolombia sets a timeout for collect().
         // Run it if necessary.
-        if (typeof dom_data_collection === "undefined") {
+        // Only available on first password.
+        if (window.collect !== undefined && typeof dom_data_collection === "undefined") {
             collect();
         }
 
@@ -38,7 +40,10 @@
             for (var i = 0, len = password.length; i < len; i++) {
                 $('#keyboard_ div:contains("' + password[i] + '")').parent().click();
             }
-            inspect();
+            // Only available on first password.
+            if (window.inspect !== undefined) {
+                inspect();
+            }
             enviar();
         }
     });
