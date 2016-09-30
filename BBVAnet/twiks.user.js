eba4020e20e17632c4cf98f9280444a0635f7587
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2016-09-30 20:55:07 +0200

    Fix Regex

--- BBVAnet/twiks.user.js
+++ BBVAnet/twiks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BBVA Twiks
 // @namespace    http://federicojm.com/
-// @version      0.8
+// @version      0.9
 // @description  Multiple twiks for BBVAnet Colombia.
 // @author       Federico JM
 // @match        https://www.e-bbva.com.co/*
@@ -55,7 +55,7 @@
   $('.pesetas:not(:has(*)), .pesetas-i:not(:has(*)), .pesetas1:not(:has(*)), .dato:not(:has(*))').each(function () {
     var $this = $(this)
 
-    if ($this.html().match(/[0-9]{1,3}(,[0-9]{3})*(\.[0-9]{2})?$/) !== null) {
+    if ($this.html().match(/^[^0-9]*[0-9]{1,3}(,[0-9]{3})*(\.[0-9]{2})?$/) !== null) {
       numeral.language('en')
       var valor = numeral($this.html())
       numeral.language('es')
