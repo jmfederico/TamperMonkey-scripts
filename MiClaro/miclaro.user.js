4c355c1a8c359365ae34efdad488e1c68774d6ca
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2017-03-11 18:36:17 -0500

    Remove Mi Claro chat

---
+++ MiClaro/miclaro.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Mi Claro - Twiks
+// @namespace    http://federicojm.com/
+// @version      0.1
+// @description  Remove annoying chat popup that ALWAYS shows up.
+// @author       Federico JM
+// @match        http://miclaro.claro.com.co/wps/myportal/co/mtmx/mi-telmex*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  'use strict'
+
+  var deleteChat = function () {
+    var chat = document.getElementById('inbenta')
+    if (chat) {
+      chat.remove()
+      clearInterval(intervalId)
+    }
+  }
+  var intervalId = setInterval(deleteChat, 500)
+})()
