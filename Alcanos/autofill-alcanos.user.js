95e3e37a97956c7a345420c2a88bcdb8f8a056ac
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2017-05-22 20:10:09 -0500

    Alcanos PSE

---
+++ Alcanos/autofill-alcanos.user.js
@@ -0,0 +1,77 @@
+// ==UserScript==
+// @name         Alcanos - Autofill bill info
+// @namespace    http://federicojm.com/
+// @version      0.1
+// @description  Autofill info for Alcanos payments
+// @author       Federico JM
+// @match        https://gateway.pagosonline.net/apps/facturacion/36715/index.html*
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+
+(function () {
+  'use strict'
+
+  window.zk.oldEndProcessing = window.zk.endProcessing
+
+  window.zk.endProcessing = function () {
+    window.zk.oldEndProcessing()
+
+    var data = JSON.parse(localStorage.getItem('tampermonkey-autofill'))
+
+    if (data && data.banco && data.nombre && data.tipo && data.documento && data.telefono && data.email) {
+      var usuario = document.querySelector('input[id$="q1"]')
+      if (usuario) {
+        usuario.value = data.usuario
+        usuario.focus()
+      }
+
+      var banco = document.querySelector('select[id$="_8"]')
+      if (banco) {
+        banco.value = data.banco
+        banco.focus()
+      }
+
+      var natural = document.querySelector('input[id$="58-real"]')
+      if (natural) {
+        natural.checked = true
+        natural.click()
+      }
+
+      var nombre = document.querySelector('input[id$="b8"]')
+      if (nombre) {
+        nombre.value = data.nombre
+        nombre.focus()
+      }
+
+      var tipo = document.querySelector('select[id$="g8"]')
+      if (tipo) {
+        tipo.value = data.tipo
+        tipo.focus()
+      }
+
+      var documento = document.querySelector('input[id$="l8"]')
+      if (documento) {
+        documento.value = data.documento
+        documento.focus()
+      }
+
+      var telefono = document.querySelector('input[id$="q8"]')
+      if (telefono) {
+        telefono.value = data.telefono
+        telefono.focus()
+      }
+
+      var email = document.querySelector('input[id$="v8"]')
+      if (email) {
+        email.value = data.email
+        email.focus()
+      }
+    } else {
+      window.console.log('--- Tampermonkey autofill ---\n\n' +
+                         'Create a local data storage with autofill values.\n' +
+                         'localStorage.setItem("tampermonkey-autofill", JSON.stringify({"usuario":"USUARIO", "banco":"BANCO","nombre":"NOMBRE","tipo":"TIPO DE DOCUMENTO","documento":"00000000","telefono":"5551234","email":"EMAIL@DOMINIO.COM"}))\n\n' +
+                         '--- Tampermonkey autofill ---')
+    }
+  }
+})()
