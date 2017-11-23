c018834152330f922cde7e614a88710a071eaf19
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2017-11-23 10:24:12 -0500

    Compatibility with MicrosoftAjax

--- Electrohuila/autofill-electrohuila.user.js
+++ Electrohuila/autofill-electrohuila.user.js
@@ -1,18 +1,18 @@
 // ==UserScript==
 // @name         Eletrohuila - Autofill bill info
 // @namespace    http://federicojm.com/
-// @version      0.1
+// @version      0.2
 // @description  Autofill info for Electrohuila payments
 // @author       Federico JM
-// @match        http://www.electrohuila.com.co/Pago_Electronico/PagosEnLinea.aspx
+// @match        http://www.electrohuila.com.co/Pago_Electronico/PagosEnLinea.aspx*
 // @grant        none
 // @run-at       document-end
 // ==/UserScript==
 
-(function () {
+fillData = function () {
   'use strict'
 
-  var data = JSON.parse(localStorage.getItem('userscript-autofill'))
+  var data = JSON.parse(localStorage.getItem('tampermonkey-autofill'))
 
   if (data && data.email && data.mobile) {
     var email = document.getElementById('dnn_ctr695_ViewGWZonaPagos_txtEmail')
@@ -24,9 +24,16 @@
       mobile.value = data.mobile
     }
   } else {
-    window.console.log('--- Userscript autofill ---\n\n' +
+    window.console.log('--- Tampermonkey autofill ---\n\n' +
                        'Create a local data storage with autofill values.\n' +
-                       'localStorage.setItem("userscript-autofill", JSON.stringify({"email":"EMAIL","mobile":"MOBILE"}))\n\n' +
-                       '--- Userscript autofill ---')
+                       'localStorage.setItem("tampermonkey-autofill", JSON.stringify({"email":"EMAIL","mobile":"MOBILE"}))\n\n' +
+                       '--- Tampermonkey autofill ---')
   }
-})()
+}
+
+try {
+    Sys.WebForms.PageRequestManager.getInstance().add_pageLoaded(fillData)
+}
+catch(TypeError) {
+    fillData()
+}
