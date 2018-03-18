fbcf44720bd80cc49bc7f37450d4f33026add116
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2018-03-18 13:19:30 -0500

    Fix code formatting

--- Gmail/print.user.js
+++ Gmail/print.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Gmail Print
 // @namespace    http://federicojm.com/
-// @version      0.2
+// @version      0.3
 // @description  Better email printing for Gmail.
 // @author       Federico JM
 // @match        https://mail.google.com/mail/*?*view=pt*
@@ -10,69 +10,67 @@
 // ==/UserScript==
 
 /* - jshint settings - */
-/* jshint esversion: 6 */
 /* globals GM_addStyle */
 
 setTimeout(function () {
-    // Deferred onload
-}, 2000);
+  // Deferred onload
+}, 2000)
 
-(function ($) {
-    'use strict';
-    var $mc = $('.maincontent');
-    var $el;
+;(function ($) {
+  'use strict'
+  var $mc = $('.maincontent')
+  var $el
 
-    $el = $mc.children('table').first();
-    $el = $el.add($mc.children('hr').first());
-    $mc.find('> .message > tbody').each(function () {
-        $el = $el.add($(this).children('tr').slice(0, 2));
-    });
+  $el = $mc.children('table').first()
+  $el = $el.add($mc.children('hr').first())
+  $mc.find('> .message > tbody').each(function () {
+    $el = $el.add($(this).children('tr').slice(0, 2))
+  })
 
-    $el.addClass('gmail-hide');
+  $el.addClass('gmail-hide')
 
-    var $toggle = $('<button>');
-    $toggle.html('Extra info - mostrar');
-    $toggle.addClass('gmail-toggle');
-    var $body = $('body');
-    $body.append($toggle);
+  var $toggle = $('<button>')
+  $toggle.html('Extra info - mostrar')
+  $toggle.addClass('gmail-toggle')
+  var $body = $('body')
+  $body.append($toggle)
 
-    var toggle = true;
-    $toggle.click(function () {
-        if (toggle) {
-            $el.removeClass('gmail-hide');
-            $toggle.html('Extra info - ocultar');
-        } else {
-            $el.addClass('gmail-hide');
-            $toggle.html('Extra info - mostrar');
-        }
-        toggle = !toggle;
-    });
+  var toggle = true
+  $toggle.click(function () {
+    if (toggle) {
+      $el.removeClass('gmail-hide')
+      $toggle.html('Extra info - ocultar')
+    } else {
+      $el.addClass('gmail-hide')
+      $toggle.html('Extra info - mostrar')
+    }
+    toggle = !toggle
+  })
 
-    GM_addStyle(`
+  GM_addStyle(`
         body {
-            margin: 0;
+            margin: 0
         }
 
         body > .bodycontainer > table,
         body > .bodycontainer > hr {
-            display: none;
+            display: none
         }
 
         .gmail-hide {
-            display: none;
+            display: none
         }
 
         .gmail-toggle {
-            position: fixed;
-            top: 5px;
-            right: 5px;
+            position: fixed
+            top: 5px
+            right: 5px
         }
 
         @media print {
             .gmail-toggle {
-                display: none;
+                display: none
             }
         }
-    `);
-
-})(jQuery);
+    `)
+})(jQuery)
