cdf9a794f81f5899d388517f89bd07999ec1bec8
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2019-09-06 11:55:07 -0500

    Use native video controls in Claro video

--- Claro/video.user.js
+++ Claro/video.user.js
@@ -1,14 +1,29 @@
 // ==UserScript==
-// @name         Claro Video - No mouse hijacking
+// @name         Claro Video - No video hijacking
 // @namespace    http://federicojm.com/
-// @version      0.1
-// @description  Disable mouse hijacking by Claro Video
+// @version      0.2
+// @description  Disable video control hijacking by Claro Video
 // @author       Federico JM
 // @match        https://www.clarovideo.com/playerhtml5/*
 // @grant        none
 // @run-at       document-end
 // ==/UserScript==
 
-document.onselectstart = null
-document.oncontextmenu = null
-document.onmousedown = null
+;(() => {
+  document.onselectstart = null
+  document.oncontextmenu = null
+  document.onmousedown = null
+
+  let intervalId = window.setInterval(() => {
+    let videoControls = document.querySelectorAll('vph5-controls')
+    if (!videoControls.length) {
+      return
+    }
+    window.clearInterval(intervalId)
+
+    videoControls.forEach(node => {
+      node.remove()
+    })
+    document.querySelector('video').controls = true
+  }, 500)
+})()
