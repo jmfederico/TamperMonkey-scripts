30cd9a8cd602b69b22c994064655e0ce398e6be9
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2020-04-08 09:48:53 +0200

    Add El Espectador - fix sticky header jump

---
+++ elespectador.com/sticky_header.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         El Espectador - Sticky header
+// @namespace    http://federicojm.com/
+// @version      0.1
+// @description  Fix sticky header jump.
+// @author       Federico JM
+// @match        https://www.elespectador.com/*
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+
+// Select the node that will be observed for mutations
+const header = document.querySelector(".l-page .l-header");
+const originalHeight = header.offsetHeight;
+
+// Create an observer instance linked to the callback function
+const observer = new MutationObserver((mutationsList, _observer) => {
+  if (mutationsList[0].target.className === mutationsList[0].oldValue) {
+    return;
+  }
+
+  const header = mutationsList[0].target;
+  const main = header.nextElementSibling;
+
+  if (header.classList.contains("header_sticky")) {
+    main.style.marginTop = originalHeight + "px";
+    return;
+  }
+
+  main.style.marginTop = null;
+});
+
+// Start observing the target node for configured mutations
+observer.observe(header, {
+  attributes: true,
+  attributeFilter: ["class"],
+  attributeOldValue: true,
+});
